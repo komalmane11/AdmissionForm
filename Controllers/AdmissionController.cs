1bcb12fe5629f22e62b5e36eac904cb163bd7a30
komalmane11 <komalmane110800@gmail.com>
2025-04-02 19:41:52 +0530

    Add files via upload

---
+++ Controllers/AdmissionController.cs
@@ -0,0 +1,33 @@
+﻿using AdmissionFormUsing_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdmissionFormUsing_MVC.Controllers
+{
+    public class AdmissionController : Controller
+    {
+        //Get
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Post Addmission
+        [HttpPost]
+        public IActionResult Submit(Admission model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Admission is successfully done!";
+                return RedirectToAction("Homepage");
+            }
+
+            return View("Index", model);
+        }
+
+        //homepage after succesfully admission
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Admission.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdmissionFormUsing_MVC.Models
+{
+    public class Admission
+    {
+        //[Required]
+        //public string FullName {  get; set; }
+
+        //[Required,EmailAddress]
+        //public string Email { get; set; }
+
+        //[Required,Phone]
+        //public double MobileNumber { get; set; }
+
+        //[Required]
+        //public string Class {  get; set; }
+
+        //[Required]
+        //public string CourseName { get; set; }
+
+        //[Required]
+        //public string Address {  get; set; }
+
+        //[Required]
+        //public string PaymentMethod {  get; set; }
+
+
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string Class { get; set; }
+        public string CourseName { get; set; }
+        public string Address { get; set; }
+        public string PaymentMethod { get; set; }
+
+    }
+}
